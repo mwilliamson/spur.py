cc403b8e397139e55ef4c7ca54ffa36088b2dc1c
Michael Williamson <mike@zwobble.org>
2012-12-03 19:51:53 +0000

    Rewrite local_test.py to allow additional shells to be added

--- tests/local_test.py
+++ tests/local_test.py
@@ -1,20 +1,33 @@
+import functools
+
 from nose.tools import istest, assert_equal
 
 from spur import LocalShell
 
-shell = LocalShell()
+def test(func):
+    @functools.wraps(func)
+    def run_test():
+        for shell in _create_shells():
+            yield func, shell
+            
+    def _create_shells():
+        return [
+            LocalShell()
+        ]
+        
+    return istest(run_test)
 
-@istest
-def output_of_run_is_stored():
+@test
+def output_of_run_is_stored(shell):
     result = shell.run(["echo", "hello"])
     assert_equal("hello\n", result.output)
 
-@istest
-def cwd_of_run_can_be_set():
+@test
+def cwd_of_run_can_be_set(shell):
     result = shell.run(["pwd"], cwd="/")
     assert_equal("/\n", result.output)
 
-@istest
-def environment_variables_can_be_added_for_run():
+@test
+def environment_variables_can_be_added_for_run(shell):
     result = shell.run(["sh", "-c", "echo $NAME"], update_env={"NAME": "Bob"})
     assert_equal("Bob\n", result.output)

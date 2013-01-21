2af7ae7de637701f15d417e5cc086c2edbe6c8e9
Michael Williamson <mike@zwobble.org>
2013-01-21 22:41:34 +0000

    Bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='spur',
-    version='0.2.2',
+    version='0.2.3',
     description='Run commands and manipulate files locally or over SSH using the same interface',
     long_description=read("README"),
     author='Michael Williamson',

4a1e84d22d7c315a7c5fb65ec607fb19425612ab
Michael Williamson <mike@zwobble.org>
2015-12-27 22:46:05 +0000

    Don't use a thread if using read call to read output

--- spur/io.py
+++ spur/io.py
@@ -4,20 +4,42 @@ import threading
 class IoHandler(object):
     def __init__(self, in_out_pairs):
         self._handlers = [
-            OutputHandler(file_in, file_out)
+            _output_handler(file_in, file_out)
             for file_in, file_out
             in in_out_pairs
         ]
         
     def wait(self):
         return [handler.wait() for handler in self._handlers]
+
+
+def _output_handler(file_in, file_out):
+    if file_out is None:
+        return _ReadOutputAtEnd(file_in)
+    else:
+        return _ContinuousReader(file_in, file_out)
+
+
+class _ReadOutputAtEnd(object):
+    def __init__(self, file_in):
+        self._file_in = file_in
+    
+    def wait(self):
+        try:
+            return self._file_in.read()
+        except IOError:
+            # TODO: is there a more elegant fix?
+            # Attempting to read from a pty master that has received nothing
+            # seems to raise an IOError when reading
+            # See: http://bugs.python.org/issue5380
+            return b""
     
 
-class OutputHandler(object):
+class _ContinuousReader(object):
     def __init__(self, file_in, file_out):
         self._file_in = file_in
         self._file_out = file_out
-        self._output = ""
+        self._output = b""
         
         self._thread = threading.Thread(target=self._capture_output)
         self._thread.daemon = True
@@ -28,22 +50,12 @@ class OutputHandler(object):
         return self._output
     
     def _capture_output(self):
-        if self._file_out is None:
-            try:
-                self._output = self._file_in.read()
-            except IOError:
-                # TODO: is there a more elegant fix?
-                # Attempting to read from a pty master that has received nothing
-                # seems to raise an IOError when reading
-                # See: http://bugs.python.org/issue5380
-                self._output = b""
-        else:
-            output_buffer = []
-            while True:
-                output = self._file_in.read(1)
-                if output:
-                    self._file_out.write(output)
-                    output_buffer.append(output)
-                else:
-                    self._output = b"".join(output_buffer)
-                    return
+        output_buffer = []
+        while True:
+            output = self._file_in.read(1)
+            if output:
+                self._file_out.write(output)
+                output_buffer.append(output)
+            else:
+                self._output = b"".join(output_buffer)
+                return

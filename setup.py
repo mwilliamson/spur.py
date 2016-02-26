3a48c5b64333b2e7409fb80feae8a83908a5ed01
Jason Kraus <zbyte64@gmail.com>
2016-02-26 14:11:42 -0800

    load readme as utf8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from distutils.core import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 setup(
     name='spur',
